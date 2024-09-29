617b79bad0b92646d619fb80f2cf4d300c586b69
YX-Gao953 <gaoyx953@hotmail.com>
2024-09-29 21:28:34 +0800

    Update Model_deploy.py

--- Model_deploy.py
+++ Model_deploy.py
@@ -19,9 +19,27 @@ from sksurv.ensemble import GradientBoostingSurvivalAnalysis
 
 ##Define user input
 # Streamlit user intersurface
-st.title("Male Breast Cancer Risk Predictor")
+st.title("Hormone Receptor-Positive Early Male Breast Cancer Risk Predictor")
+
+#Add description
+st.markdown("""
+    This model is designed to predict the 5-year overall survival rate for male patients with hormone receptor-positive early breast cancer and to stratify based on the prediction results. 
+    
+    Please enter the patient data in the input boxes below.
+    
+    Note: Although HER-2 status is not considered when constructing the model, it is still recommended for use only in predicting HER-2 negative patients. The first four variables assess the preconditions; if the requirements of the model are not met, you will receive a corresponding notification.
+    
+    If the prediction result indicates LOW-RISK, this suggests that chemotherapy may not improve the patient's prognosis. Therefore, chemotherapy omission may be considered.
+            
+    This model is intended to ASSIST physicians in making judgments.
+""")
 
 # Define input
+ERstatus = st.selectbox("Estrogen receptor (ER) status:", options=['Positive', 'Negative', 'Unknown/Borderline'])
+PRstatus = st.selectbox("Progesterone receptor (PR) status:", options=['Positive', 'Negative', 'Unknown/Borderline'])
+HER2status = st.selectbox("Human epidermal growth factor receptor-2 (HER-2) status:", options=['Positive', 'Negative', 'Unknown/Borderline'])
+Mstage = st.selectbox("M stage:", options=[0, 1])
+
 age = st.number_input("Age:", min_value=18, max_value=100)
 Tstage = st.selectbox("T stage:", options=[1, 2, 3, 4])
 Nstage = st.selectbox("N stage:", options=[0, 1, 2, 3])
@@ -33,38 +51,41 @@ Chemotherapy = st.selectbox("Chemotherapy:", options=['No', 'Yes'])
 
 
 ##Form the input data
-columns = ['Age', 'Histology=2', 'Grade=2', 'Grade=3', 'T stage=2', 'T stage=3',
+columns = ['ERstatus', 'PRstatus', 'HER2status', 'M stage', 'Age', 'Histology=2', 'Grade=2', 'Grade=3', 'T stage=2', 'T stage=3',
            'T stage=4', 'N stage=1', 'N stage=2', 'N stage=3', 'Breast surgery=1',
            'Breast surgery=2', 'Chemotherapy=1']
 
 input_df = pd.DataFrame(np.zeros((1, len(columns))), columns=columns)
 
+input_df_precondition = input_df.iloc[:, :4]
+input_df_variables = input_df.iloc[:, 4:] 
+
 # Fill the input data into empty dataframe
-input_df['Age'] = age
+input_df_variables['Age'] = age
 if Histology == 'Other':
-    input_df['Histology=2'] = 1
+    input_df_variables['Histology=2'] = 1
 if Grade == 'II':
-    input_df['Grade=2'] = 1
+    input_df_variables['Grade=2'] = 1
 elif Grade == 'III/IV':
-    input_df['Grade=3'] = 1
+    input_df_variables['Grade=3'] = 1
 if Tstage == 2:
-    input_df['T stage=2'] = 1
+    input_df_variables['T stage=2'] = 1
 elif Tstage == 3:
-    input_df['T stage=3'] = 1
+    input_df_variables['T stage=3'] = 1
 elif Tstage == 4:
-    input_df['T stage=4'] = 1
+    input_df_variables['T stage=4'] = 1
 if Nstage == 1:
-    input_df['N stage=1'] = 1
+    input_df_variables['N stage=1'] = 1
 elif Nstage == 2:
-    input_df['N stage=2'] = 1
+    input_df_variables['N stage=2'] = 1
 elif Nstage == 3:
-    input_df['N stage=3'] = 1
+    input_df_variables['N stage=3'] = 1
 if Breastsurgery == 'Mastectomy':
-    input_df['Breast surgery=1'] = 1
+    input_df_variables['Breast surgery=1'] = 1
 elif Breastsurgery == 'Partial mastectomy':
-    input_df['Breast surgery=2'] = 1
+    input_df_variables['Breast surgery=2'] = 1
 if Chemotherapy == 'Yes':
-    input_df['Chemotherapy=1'] = 1
+    input_df_variables['Chemotherapy=1'] = 1
 
 
 
@@ -73,14 +94,19 @@ model = joblib.load('Gradient_boosting_best_model.pkl')
 cut_off = 0.21830
 
 if st.button("Predict"):
-    chf_funcs = model.predict_cumulative_hazard_function(input_df.values, return_array=False)
-    times = np.arange(12, 121, 6, dtype=int)
-    risk_scores = np.row_stack([chf(times) for chf in chf_funcs])
-    risk_scores_5y = risk_scores[:, 8]
-    overall_survival_5y = (1 - risk_scores_5y) * 100
-    if risk_scores_5y <= cut_off:
-        advice = f'The prediction result is LOW-RISK. The 5-year overall survival rate is predicted to be {overall_survival_5y[0]:.1f}%.'
+    if (input_df['M stage'].iloc[0] == 1 or
+    (input_df['ERstatus'].iloc[0] != 'Positive' and input_df['PRstatus'].iloc[0] != 'Positive') or
+    input_df['HER2status'].iloc[0] != 'Negative'):
+        advice = '''The model is not available for this patient.
+        It is indicated for male patients with ER and/or PR-positive, HER-2 negative early breast cancer.'''
     else:
-        advice = f'The prediction result is HIGH-RISK. The 5-year overall survival rate is predicted to be {overall_survival_5y[0]:.1f}%.'
-
-    st.write(advice)
+        chf_funcs = model.predict_cumulative_hazard_function(input_df_variables.values, return_array=False)
+        times = np.arange(12, 121, 6, dtype=int)
+        risk_scores = np.row_stack([chf(times) for chf in chf_funcs])
+        risk_scores_5y = risk_scores[:, 8]
+        overall_survival_5y = (1 - risk_scores_5y) * 100
+        if risk_scores_5y <= cut_off:
+            advice = f'The prediction result is LOW-RISK. The 5-year overall survival rate is predicted to be {overall_survival_5y[0]:.1f}%.'
+        else:
+            advice = f'The prediction result is HIGH-RISK. The 5-year overall survival rate is predicted to be {overall_survival_5y[0]:.1f}%.'
+st.write(advice)
