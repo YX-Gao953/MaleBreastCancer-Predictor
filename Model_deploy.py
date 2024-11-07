eb85efb4adc0f7d7297a252a00b212e7e838a739
YX-Gao953 <gaoyx953@hotmail.com>
2024-11-07 22:33:31 +0800

    Update Model_deploy.py

--- Model_deploy.py
+++ Model_deploy.py
@@ -46,7 +46,6 @@ Nstage = st.selectbox("N stage:", options=[0, 1, 2, 3])
 Histology = st.selectbox("Histology:", options=['Ductal', 'Other'])
 Grade = st.selectbox("Grade:", options=['I', 'II', 'III/IV'])
 Breastsurgery = st.selectbox("Breast surgery:", options=['No', 'Mastectomy', 'Partial mastectomy'])
-Chemotherapy = st.selectbox("Chemotherapy:", options=['No', 'Yes'])
 
 
 
@@ -55,57 +54,57 @@ columns = ['ERstatus', 'PRstatus', 'HER2status', 'Mstage', 'Age', 'Histology=2',
            'T stage=4', 'N stage=1', 'N stage=2', 'N stage=3', 'Breast surgery=1',
            'Breast surgery=2', 'Chemotherapy=1']
 
-input_df = pd.DataFrame(np.zeros((1, len(columns))), columns=columns)
+input_df_nochemo = pd.DataFrame(np.zeros((1, len(columns))), columns=columns)
 
 # Fill the input data into empty dataframe
 if ERstatus == 'Positive':
-    input_df['ERstatus'] = 1
+    input_df_nochemo['ERstatus'] = 1
 elif ERstatus == 'Negative':
-    input_df['ERstatus'] = 0
-else: input_df['ERstatus'] = 999
+    input_df_nochemo['ERstatus'] = 0
+else: input_df_nochemo['ERstatus'] = 999
 
 if PRstatus == 'Positive':
-    input_df['PRstatus'] = 1
+    input_df_nochemo['PRstatus'] = 1
 elif PRstatus == 'Negative':
-    input_df['PRstatus'] = 0
-else: input_df['PRstatus'] = 999
+    input_df_nochemo['PRstatus'] = 0
+else: input_df_nochemo['PRstatus'] = 999
 
 if HER2status == 'Positive':
-    input_df['HER2status'] = 1
+    input_df_nochemo['HER2status'] = 1
 elif HER2status == 'Negative':
-    input_df['HER2status'] = 0
-else: input_df['HER2status'] = 999
+    input_df_nochemo['HER2status'] = 0
+else: input_df_nochemo['HER2status'] = 999
 
 if Mstage == 1:
-    input_df['Mstage'] = 1
-else: input_df['Mstage'] = 0
+    input_df_nochemo['Mstage'] = 1
+else: input_df_nochemo['Mstage'] = 0
 
-input_df['Age'] = age
+input_df_nochemo['Age'] = age
 if Histology == 'Other':
-    input_df['Histology=2'] = 1
+    input_df_nochemo['Histology=2'] = 1
 if Grade == 'II':
-    input_df['Grade=2'] = 1
+    input_df_nochemo['Grade=2'] = 1
 elif Grade == 'III/IV':
-    input_df['Grade=3'] = 1
+    input_df_nochemo['Grade=3'] = 1
 if Tstage == 2:
-    input_df['T stage=2'] = 1
+    input_df_nochemo['T stage=2'] = 1
 elif Tstage == 3:
-    input_df['T stage=3'] = 1
+    input_df_nochemo['T stage=3'] = 1
 elif Tstage == 4:
-    input_df['T stage=4'] = 1
+    input_df_nochemo['T stage=4'] = 1
 if Nstage == 1:
-    input_df['N stage=1'] = 1
+    input_df_nochemo['N stage=1'] = 1
 elif Nstage == 2:
-    input_df['N stage=2'] = 1
+    input_df_nochemo['N stage=2'] = 1
 elif Nstage == 3:
-    input_df['N stage=3'] = 1
+    input_df_nochemo['N stage=3'] = 1
 if Breastsurgery == 'Mastectomy':
-    input_df['Breast surgery=1'] = 1
+    input_df_nochemo['Breast surgery=1'] = 1
 elif Breastsurgery == 'Partial mastectomy':
-    input_df['Breast surgery=2'] = 1
-if Chemotherapy == 'Yes':
-    input_df['Chemotherapy=1'] = 1
+    input_df_nochemo['Breast surgery=2'] = 1
 
+input_df_chemo = input_df_nochemo
+input_df_chemo = input_df_chemo['Chemotherapy=1'] = 1
 
 precondition_columns = ['ERstatus', 'PRstatus', 'HER2status', 'Mstage']
 variables_columns = ['Age', 'Histology=2', 'Grade=2', 'Grade=3', 
@@ -113,8 +112,9 @@ variables_columns = ['Age', 'Histology=2', 'Grade=2', 'Grade=3',
                      'N stage=1', 'N stage=2', 'N stage=3', 
                      'Breast surgery=1', 'Breast surgery=2', 
                      'Chemotherapy=1']
-input_df_precondition = input_df[precondition_columns]
-input_df_variables = input_df[variables_columns]
+input_df_precondition = input_df_nochemo[precondition_columns]
+input_df_nochemo_variables = input_df_nochemo[variables_columns]
+input_df_chemo_variables = input_df_chemo[variables_columns]
 
 ##Load the model and start prediction
 model = joblib.load('Gradient_boosting_best_model.pkl')
@@ -125,15 +125,26 @@ if st.button("Predict"):
     (input_df_precondition['ERstatus'].iloc[0] != 1 and input_df_precondition['PRstatus'].iloc[0] != 1) or
     input_df_precondition['HER2status'].iloc[0] != 0):
         advice = '''The model is not available for this patient.
-        It is indicated for male patients with ER and/or PR-positive, HER-2 negative early breast cancer.'''
+        It is indicated for men with ER and/or PR-positive, HER-2 negative early breast cancer.'''
     else:
-        chf_funcs = model.predict_cumulative_hazard_function(input_df_variables.values, return_array=False)
+        chf_funcs_nochemo = model.predict_cumulative_hazard_function(input_df_nochemo_variables.values, return_array=False)
+        chf_funcs_chemo = model.predict_cumulative_hazard_function(input_df_chemo_variables.values, return_array=False)
+
         times = np.arange(12, 121, 6, dtype=int)
-        risk_scores = np.row_stack([chf(times) for chf in chf_funcs])
-        risk_scores_5y = risk_scores[:, 8][0]
-        overall_survival_5y = np.exp(-risk_scores_5y) * 100
-        if risk_scores_5y <= cut_off:
-            advice = f'The prediction result is LOW-RISK. The 5-year overall survival rate is predicted to be {overall_survival_5y:.1f}%.'
+
+        risk_scores_nochemo = np.row_stack([chf(times) for chf in chf_funcs_nochemo])
+        risk_scores_5y_nochemo = risk_scores_nochemo[:, 8][0]
+        overall_survival_5y_nochemo = np.exp(-risk_scores_5y_nochemo) * 100
+
+        risk_scores_chemo = np.row_stack([chf(times) for chf in chf_funcs_chemo])
+        risk_scores_5y_chemo = risk_scores_chemo[:, 8][0]
+        overall_survival_5y_chemo = np.exp(-risk_scores_5y_chemo) * 100
+        if risk_scores_5y_nochemo <= cut_off:
+            advice = f'''The prediction result is LOW-RISK. It suggests the patient may NOT achieve significant overall survival benefit from chemotherapy. 
+            WITHOUT chemotherapy, the 5-year overall survival rate is predicted to be {overall_survival_5y_nochemo:.1f}%.
+            With chemotherapy, the 5-year overall survival rate is predicted to be {overall_survival_5y_chemo:.1f}%.'''
         else:
-            advice = f'The prediction result is HIGH-RISK. The 5-year overall survival rate is predicted to be {overall_survival_5y:.1f}%.'
+            advice = f'''The prediction result is HIGH-RISK. It suggests the patient may achieve significant overall survival benefit from chemotherapy. 
+            WITHOUT chemotherapy, the 5-year overall survival rate is predicted to be {overall_survival_5y_nochemo:.1f}%.
+            With chemotherapy, the 5-year overall survival rate is predicted to be {overall_survival_5y_chemo:.1f}%.'''
 st.write(advice)
